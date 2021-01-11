84f188e8c45bf7572526e2d1d89972be036b2d3b
Yohan <cepoangkuningan@gmail.com>
2021-01-11 23:01:59 +0700

    update file on this file

--- PraktekString2.py
+++ PraktekString2.py
@@ -15,9 +15,9 @@ kata_positif_jeruk = 0
 kata_positif_salak = 0
 for judul in judul_artikel: 
     for kata in kata_positif:
-        if judul.count("Jeruk") and kata_positif: 
+        if judul.count("Jeruk") > 0 and judul.count(kata) > 0: 
             kata_positif_jeruk += 1
-        if judul.count("Salak") and kata_positif:
+        if judul.count("Salak") > 0 and judul.count(kata) > 0:
             kata_positif_salak += 1
 print(kata_positif_jeruk) 
 print(kata_positif_salak)
