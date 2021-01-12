7550ee6d90f0f27b1daef585f2021a5b56d389ef
Yohan <cepoangkuningan@gmail.com>
2021-01-12 21:04:18 +0700

    Change var Teks to txt

--- SStartOrEndUsingSubString.py
+++ SStartOrEndUsingSubString.py
@@ -1,15 +1,15 @@
-'''
+"""
 Menentukan String Apakah Diawali/Diakhiri oleh Sub-string
-Pada bagian ini, aku akan mempelajari 
+Pada bagian ini, aku akan mempelajari
 bagaimana menentukan apakah suatu string diawali atau diakhiri dengan
 suatu substring (teks) tertentu.
 startswith() -> Mengembalikan nilai kebenaran True ketika sebuah teks (string) diawali dengan sebuah teks lainnya.
 endswith() -> Mengembalikan nilai kebenaran True ketika sebuah teks (string) diakhiri dengan sebuah teks lainnya.
-'''
+"""
 print(">>> Fitur .startswith()")
-teks = "Apel malang adalah apel termanis dibanding apel-apel lainnya"
-print(teks.startswith("Apel"))
-print(teks.startswith("apel"))
-print(">>> Fitur .endswith()")
-print(teks.endswith("lainnya"))
-print(teks.endswith("apel"))
+txt = "Apel malang adalah apel termanis dibanding apel-apel lainnya"
+print(txt.startswith("Apel"))
+print(txt.startswith("apel"))
+print(">>> Feature .endswith()")
+print(txt.endswith("lainnya"))
+print(txt.endswith("apel"))
